9ef0907faeb80b300909ea41c723945dd65dcaf2
Eco Villaraza <jerico.villaraza19@gmail.com>
2021-09-24 22:51:40 +0800

    Deymsan

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.TESTING_DATABASE).then(() => {
 });
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(UtilsRouter);
 
