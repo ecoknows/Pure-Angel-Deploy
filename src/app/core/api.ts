5740b80eabf47e5f1acb41fb4258f2149bd17317
Eco Villaraza <jerico.villaraza19@gmail.com>
2021-09-25 01:30:56 +0800

    gasdg

--- src/app/core/api.ts
+++ src/app/core/api.ts
@@ -1,2 +1,3 @@
-export const SERVER_URL = 'http://localhost:3000/';
+// export const SERVER_URL = 'http://localhost:3000/';
 // export const SERVER_URL = 'https://pure-angel-cofee-testnet.herokuapp.com/api';
+export const SERVER_URL = '';
