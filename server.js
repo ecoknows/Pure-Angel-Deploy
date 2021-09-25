e9d0ab1ca63322065fc8c5063b770bf0b3e39195
Eco Villaraza <jerico.villaraza19@gmail.com>
2021-09-25 10:38:53 +0800

    Deymsan

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/dist/pure-angel-coffee"));
 
-app.get("*", function (req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/pure-angel-coffee/index.html"));
 });
 
