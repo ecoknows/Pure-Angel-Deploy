c0f8ed3b92ca0a8dba33bcaf412ec1f56c83a330
Eco Villaraza <jerico.villaraza19@gmail.com>
2021-09-25 11:48:16 +0800

    Deymsan

--- src/app/core/api.ts
+++ src/app/core/api.ts
@@ -1,3 +1,3 @@
-// export const SERVER_URL = 'http://localhost:3000/';
+export const SERVER_URL = 'http://localhost:3000/';
 // export const SERVER_URL = 'https://pure-angel-cofee-testnet.herokuapp.com/api';
-export const SERVER_URL = '';
+// export const SERVER_URL = '';
